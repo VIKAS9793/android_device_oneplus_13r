f15d1b683a88cf6f8659c38ec819a01a5ca4998b
VIKAS9793 <vikassahani17@gmail.com>
2025-04-03 20:57:38 +0530

    Initial commit: OnePlus 13R device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,109 @@
+# Inherit from the proprietary version
+-include vendor/oneplus/13r/BoardConfigVendor.mk
+
+DEVICE_PATH := device/oneplus/13r
+
+# A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    odm \
+    product \
+    system \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor \
+    vendor_boot \
+    vendor_dlkm
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := kryo385
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := kryo385
+
+# Bootloader
+PRODUCT_PLATFORM := taro
+TARGET_BOOTLOADER_BOARD_NAME := taro
+TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+# Kernel - prebuilt
+TARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
+BOARD_DTBOIMG_PARTITION_SIZE := 25165824
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 100663296
+
+# Dynamic Partitions
+BOARD_SUPER_PARTITION_SIZE := 9126805504
+BOARD_SUPER_PARTITION_GROUPS := oneplus_dynamic_partitions
+BOARD_ONEPLUS_DYNAMIC_PARTITIONS_SIZE := 9122611200
+BOARD_ONEPLUS_DYNAMIC_PARTITIONS_PARTITION_LIST := \
+    odm \
+    product \
+    system \
+    system_ext \
+    vendor \
+    vendor_dlkm
+
+# File systems
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Recovery
+BOARD_USES_RECOVERY_AS_BOOT := true
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2024-02-01
+
+# VINTF
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+
+# Platform
+TARGET_BOARD_PLATFORM := taro
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno730
+
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/sys/class/touch/touch_dev/gesture_on"
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
+# Inherit the proprietary files
+include vendor/oneplus/13r/BoardConfigVendor.mk 
---
+++ README.md
@@ -0,0 +1,26 @@
+# OnePlus 13R Device Tree
+
+## Device specifications
+
+| Feature                 | Specification                     |
+| :---------------------- | :-------------------------------- |
+| CPU                     | Octa-core (1x2.4 GHz Cortex-A78 & 3x2.36 GHz Cortex-A78 & 4x1.8 GHz Cortex-A55) |
+| Chipset                 | Qualcomm SM8475 Snapdragon 8+ Gen 1 (4 nm) |
+| GPU                     | Adreno 730 |
+| Memory                  | 8GB/12GB/16GB LPDDR5 |
+| Shipped Android Version | 14.0 |
+| Storage                 | 128GB/256GB UFS 3.1 |
+| Battery                 | 5000 mAh |
+| Display                 | 6.74 inches, 1240 x 2772 pixels, AMOLED, 120Hz |
+| Camera (Rear)           | 50 MP (wide), 8 MP (ultrawide), 2 MP (macro) |
+| Camera (Front)          | 16 MP (wide) |
+
+## Copyright
+
+```
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+``` 
