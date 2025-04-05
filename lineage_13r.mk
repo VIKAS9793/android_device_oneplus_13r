8f3bbb64873dd9cda9f7d7ac2747ec0381fe1b92
VIKAS9793 <your.email@example.com>
2025-04-05 19:53:30 +0530

    device: 13r: Inherit from sm8650-common device tree

--- lineage_13r.mk
+++ lineage_13r.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from 13r device
 $(call inherit-product, device/oneplus/13r/device.mk)
 
+# Inherit from sm8650-common
+$(call inherit-product, device/oneplus/sm8650-common/common.mk)
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
